bbf3c41537fc091ec55b10d1ae318329a38c1939
twkelwin <twkelwin@gmail.com>
2025-04-28 20:55:14 -0300

    Atulizando contato na classe Main

--- src/br/com/agenda/aplicacao/Main.java
+++ src/br/com/agenda/aplicacao/Main.java
@@ -11,24 +11,27 @@ public class Main {
 		ContatoDAO contatoDao = new ContatoDAO();
 
 		Contato contato = new Contato();
-		contato.setNome("Joao");
+		contato.setNome("Joao gabriel");
 		contato.setIdade(44);
 		contato.setDataCadastro(new Date());
 
 		contatoDao.save(contato);
 
-		Contato contato2 = new Contato();
-		contato2.setNome("kelwinho");
-		contato2.setIdade(56);
-		contato2.setDataCadastro(new Date());
-
-		contatoDao.save(contato2);
+		//Atualizar o contato
+		Contato c1 = new Contato();
+		c1.setNome("Joao gabriel Silva");
+		c1.setIdade(45);
+		c1.setDataCadastro(new Date());
+		c1.setId(8); //é o ID do banco de dados
+		
+		contatoDao.update(c1);
 
 		// vizualização dos registros do banco de dados (TODOS)
 
 		for (Contato c : contatoDao.getContatos()) {
 			System.out.println("Contatos: " + c.getNome());
 		}
+		
 
 	}
 
